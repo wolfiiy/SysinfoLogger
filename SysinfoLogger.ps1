442249f4ff5af5441ba03c30e8d05ed480ab5310
wolfiy <wolfiy@protonmail.ch>
2024-01-26 15:40:55 +0100

    Disk info

--- SysinfoLogger.ps1
+++ SysinfoLogger.ps1
@@ -64,7 +64,7 @@ $FilePath = "sysinfo.log"
 ###################################################################################################################
 # Body
 
-$date = Get-Date -Format "yyyy/MM/dd HH:mm"
+$date = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
 $Hostname = (Get-CimInstance CIM_ComputerSystem).Name
 
 $OS = (Get-CimInstance -ClassName CIM_OperatingSystem)
@@ -73,6 +73,8 @@ $OSVersion = $OS.Version
 $OSBuild = $OS.BuildNumber
 
 $CPU = (Get-CimInstance -ClassName CIM_Processor).Name
+$GPU = (Get-CimInstance -ClassName CIM_VideoController).Name
+$LDisks = Get-CimInstance -ClassName CIM_LogicalDisk
 
 
 function Write-Data() {
@@ -81,13 +83,23 @@ function Write-Data() {
     Write-Host Hostname: `t$Hostname
     Write-Host OS: `t`t$OSName
     Write-Host Version: `t$OSVersion Build $OSBuild
-    Write-Host CPU `t`t$CPU
-    $GPU = (Get-CimInstance -ClassName CIM_VideoController).Name
     
+    Write-Host CPU `t`t$CPU
     $i = 0
     foreach($VGA in $GPU) {
         Write-Host GPU $i`: `t`t$VGA
         $i++
+    }
+
+    foreach($Disk in $LDisks){
+        $DiskName = $Disk.DeviceID
+        $DiskSize = [math]::Round($Disk.Size / 1gb)
+        $DiskFree = [math]::Round($Disk.FreeSpace / 1gb)
+
+        if ($DiskSize -gt 0) {
+            $DiskFreePercent = [math]::Round($DiskFree / $DiskSize * 100, 2)
+            Write-Host $DiskName `t`t$DiskFree / $DiskSize Gb`, $DiskFreePercent% free
+        }
     }
 }
 
@@ -98,6 +110,17 @@ function Write-To-File() {
     Write-Output "OS: `t`t$OSName" >> $FilePath
     Write-Output "Version: `t$OSVersion Build $OSBuild" >> $FilePath
     Write-Output "CPU `t`t$CPU" >> $FilePath
+    # TODO store in variable
+    foreach($Disk in $LDisks){
+        $DiskName = $Disk.DeviceID
+        $DiskSize = [math]::Round($Disk.Size / 1gb)
+        $DiskFree = [math]::Round($Disk.FreeSpace / 1gb)
+
+        if ($DiskSize -gt 0) {
+            $DiskFreePercent = [math]::Round($DiskFree / $DiskSize * 100, 2)
+            Write-Output "$DiskName `t`t$DiskFree / $DiskSize Gb`, $DiskFreePercent% free" >> $FilePath
+        }
+    }
 }
 
 Write-Data
